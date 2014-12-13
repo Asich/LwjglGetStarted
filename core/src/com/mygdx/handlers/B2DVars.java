57cce41007b70a7a2a107dfe9b9ca68aa39ecc1d
mustafin <mustafinmurat@gmail.com>
2014-12-13 19:53:33 +0600

    Commit

---
+++ core/src/com/mygdx/handlers/B2DVars.java
@@ -0,0 +1,8 @@
+package com.mygdx.handlers;
+
+/**
+ * Created by Murat on 13.12.2014.
+ */
+public class B2DVars {
+    public static final float PPM = 100;
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = MyGame.TITLE;
+        config.width = MyGame.V_WIDTH * MyGame.SCALE;
+        config.height = MyGame.V_HEIGHT * MyGame.SCALE;
 		new LwjglApplication(new MyGame(), config);
 	}
 }
